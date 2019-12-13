e3f56bf71fa2c6c7ae49d4bc58dd7f5cdb4d8de6
Administrator <646066936@qq.com>
2019-12-13 12:27:26 +0800

    eureka服务注册与发现

---
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
