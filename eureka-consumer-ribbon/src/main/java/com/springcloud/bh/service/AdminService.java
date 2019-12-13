c709207da635d94ce649832066ed3ab396a7a893
Administrator <646066936@qq.com>
2019-12-13 14:43:49 +0800

    eureka-consumer-ribbon 增加Hystrix服务熔断，防止服务雪崩

--- eureka-consumer-ribbon/src/main/java/com/springcloud/bh/service/AdminService.java
+++ eureka-consumer-ribbon/src/main/java/com/springcloud/bh/service/AdminService.java
@@ -1,7 +1,7 @@
 package com.springcloud.bh.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,9 +12,15 @@ public class AdminService {
    private RestTemplate restTemplate;
 
 
+   @HystrixCommand(fallbackMethod ="hiError" )
    public String sayHi(String message)
    {
        return restTemplate.getForObject("http://eureka-client/hi?message="+message,String.class);
+   }
+
+   public String hiError(String message)
+   {
+      return String.format("你的消息是：%s ，但是请求连接失败！",message);
    }
 
 }
