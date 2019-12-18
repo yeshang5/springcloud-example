8bc2a1bc88ef0cffc350d6f608ac6cb4d79e74fa
Administrator <646066936@qq.com>
2019-12-18 16:38:11 +0800

    链路追踪

--- eureka-consumer-feign/src/main/java/com/springcloud/bh/service/AdminService.java
+++ eureka-consumer-feign/src/main/java/com/springcloud/bh/service/AdminService.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface AdminService {
 
     @GetMapping("hi")
+
     public String sayHi(@RequestParam("message")String message);  //需使用@RequestParam，否则报405错误
 
 }
